1db40c339cdace0e1812b915e54c37f3ec9bc412
Naren Sathiya <narensathiya92@gmail.com>
2017-07-25 12:44:03 -0700

    added PQ and node, and some tests

--- build/Cache.js
+++ build/Cache.js
@@ -1,17 +1,10 @@
-import React, { Component } from "react";
-import { Image, Platform } from "react-native";
 import RNFetchBlob from "react-native-fetch-blob";
-
-import { Node } from '../build/Node';
-import { PriorityQueue } from '../build/Queue';
-
 const SHA1 = require("crypto-js/sha1");
 const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 const BASE_DIR = RNFetchBlob.fs.dirs.CacheDir + "/image-cache/files";
-const QUEUE_DIR = RNFetchBlob.fs.dirs.CacheDir + "/image-cache/queue.json";
+const QUEUE_DIR = RNFetchBlob.fs.dirs.CacheDir + "/image-cache";
 const CACHE_LIMIT_FILE_COUNT = 10;
 const FILE_PREFIX = Platform.OS === "ios" ? "" : "file://";
-
 
 export class ImageCache {
 
@@ -19,34 +12,20 @@ export class ImageCache {
       return new Promise((resolve, reject) => {
         RNFetchBlob.fs.readFile(QUEUE_DIR, 'base64')
         .then((data) => {
-            if (data) {
-                console.log('data', data)
-                resolve(data)
+            if (exists) {
+                this.notify(dbPath);
             }
             else {
-                console.log('data', data)
+                this.download(cache);
             }
         })
         .catch((err) => {
-         console.log('err', err);
+         console.log(err);
         });
       })
     }
-
-    constructQueueAndMap(data){
-      let queue = new PriorityQueue();
-      const map = {};
-      data.forEach((d) => {
-        const n = new Node(d)
-        queue.enqueue(n)
-        map[d] = n;
-      })
-      return {queue, map}
-    }
-
-    constructor(data) {
+    constructor() {
         this.cache = {};
-        {this.queue, this.map} = constructQueueAndMap(data)
     }
     getPath(dbPath, immutable) {
         let path = dbPath.substring(dbPath.lastIndexOf("/"));
@@ -67,9 +46,9 @@ export class ImageCache {
         .catch((err) => reject(err))
       })
     }
-    static get(data) {
+    static get() {
         if (!ImageCache.instance) {
-            ImageCache.instance = new ImageCache(data);
+            ImageCache.instance = new ImageCache();
         }
         return ImageCache.instance;
     }
@@ -172,82 +151,3 @@ export class ImageCache {
         });
     }
 }
-export class BaseCachedImage extends Component {
-    constructor() {
-        super();
-        this.handler = (path) => {
-            this.setState({ path });
-        };
-        this.state = { path: undefined };
-    }
-    dispose() {
-        if (this.dbPath) {
-            ImageCache.get().dispose(this.dbPath, this.handler);
-        }
-    }
-    observe(source, mutable) {
-        if (source.dbPath !== this.dbPath) {
-            this.dispose();
-            this.dbPath = source.dbPath;
-            ImageCache.get().on(source, this.handler, !mutable);
-        }
-    }
-    getProps() {
-        const props = {};
-        Object.keys(this.props).forEach(prop => {
-            if (prop === "source" && this.props.source.dbPath) {
-                props["source"] = this.state.path ? { dbPath: this.props.source.dbPath, dbProvider: this.props.source.dbProvider, uri: FILE_PREFIX + this.state.path } : {};
-            }
-            else if (["mutable", "component"].indexOf(prop) === -1) {
-                props[prop] = this.props[prop];
-            }
-        });
-        return props;
-    }
-    checkSource(source) {
-        if (Array.isArray(source)) {
-            throw new Error(`Giving multiple URIs to CachedImage is not yet supported.
-            If you want to see this feature supported, please file and issue at
-             https://github.com/wcandillon/react-native-img-cache`);
-        }
-        return source;
-    }
-    componentWillMount() {
-        const { mutable } = this.props;
-        const source = this.checkSource(this.props.source);
-        if (source.dbPath) {
-            this.observe(source, mutable === true);
-        }
-    }
-    componentWillReceiveProps(nextProps) {
-        const { mutable } = nextProps;
-        const source = this.checkSource(nextProps.source);
-        if (source.dbPath) {
-            this.observe(source, mutable === true);
-        }
-    }
-    componentWillUnmount() {
-        this.dispose();
-    }
-}
-export class CachedImage extends BaseCachedImage {
-    constructor() {
-        super();
-    }
-    render() {
-        const props = this.getProps();
-        return React.createElement(Image, Object.assign({}, props), this.props.children);
-    }
-}
-export class CustomCachedImage extends BaseCachedImage {
-    constructor() {
-        super();
-    }
-    render() {
-        const { component } = this.props;
-        const props = this.getProps();
-        const Component = component;
-        return React.createElement(Component, Object.assign({}, props), this.props.children);
-    }
-}
-//# sourceMappingURL=index.js.map
---
+++ build/Queue.js
@@ -0,0 +1,60 @@
+const Node = require('./Node').Node;
+
+export class PriorityQueue {
+
+  constructor() {
+      this.queue = [];
+      this.currentSize = 0;
+      this.head = new Node();
+      this.tail = new Node();
+  }
+
+  remove(entry){
+    entry.prev.next = entry.next;
+    entry.next.prev = entry.prev;
+    entry.next = null;
+    entry.prev = null;
+    this.currentSize--;
+  }
+
+  enqueue(entry){
+    if (this.currentSize == 0){
+      this.head.next = entry;
+      this.tail.prev = entry;
+      entry.next = this.tail;
+      entry.prev = this.head;
+    } else {
+      entry.next = this.head.next;
+      this.head.next.prev = entry;
+      this.head.next = entry;
+      entry.prev = this.head;
+    }
+    this.currentSize++;
+  }
+
+  dequeue(){
+    let toRemove = this.tail.prev;
+    this.tail.prev = toRemove.prev;
+    toRemove.prev.next = this.tail;
+    toRemove.prev = null;
+    toRemove.next = null;
+    this.currentSize--;
+    return toRemove;
+  }
+
+  getSize(){
+    return this.currentSize;
+  }
+
+  print(){
+    let curr = this.head.next;
+    let str = "";
+    while (curr != this.tail){
+      str += curr.val + "->"
+      curr = curr.next;
+    }
+    console.log('Q: ', str);
+    return str;
+  }
+
+}
